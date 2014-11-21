f285df5047ef4df1ba95945573ed042deddc30f0
Susumu-Yoshikawa <susumu_yoshikawa@tbn.co.jp>
2014-11-21 17:31:59 +0900

    mybatis対応

---
+++ salesManage/src/main/java/com/sales/Automobile/Automobile.java
@@ -0,0 +1,57 @@
+package com.sales.Automobile;
+
+public class Automobile {
+	private int id;
+    private String make;
+    private String model;
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Automobile() {
+        super();
+    }
+
+    public Automobile( int id, String make, String model,int year) {
+        super();
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Automobile{" + "id=" + id + ",make=" + make + ",model=" + model + ",year=" + year + '}';
+    }
+}
--- salesManage/src/main/java/com/tbn/sales/HomeController.java
+++ salesManage/src/main/java/com/tbn/sales/HomeController.java
@@ -1,15 +1,23 @@
 package com.tbn.sales;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sales.Automobile.Automobile;
 
 /**
  * Handles requests for the application home page.
@@ -21,9 +29,10 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * @throws IOException 
 	 */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model) throws IOException {
 		logger.info("Welcome home! The client locale is!! {}.", locale);
 		
 		Date date = new Date();
@@ -32,6 +41,22 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		
+		Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        SqlSession session = new SqlSessionFactoryBuilder().build(reader).openSession();
+        Automobile sample1 = (Automobile)session.selectOne("select", 12);
+        System.out.println(sample1);
+        // List
+        List<Automobile> sample2 = (List<Automobile>)session.selectList("select_all");
+        for (Automobile a : sample2) {
+            System.out.println("a = [" + a + "]");
+        }
+        // Insert
+        Automobile insert_data = new Automobile(12,"bbb" , "foo" , 19790114);
+        session.insert("insert", insert_data);
+
+        session.commit();
 		
 		return "home";
 	}
